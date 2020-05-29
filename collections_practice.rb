5ad39de1c7062d02933e3573958d2ebfae343719
Samantha Wabi <swfullpotential@gmail.com>
2020-05-29 01:14:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,16 +4,3 @@ end
 def sort_array_desc(array)
   array.sort.reverse
 end
-def count_elements(array)
-  new_array = array.uniq
-  new_array.each do |hash|
-    count = 0
-    array.each do |element|
-      if hash == element
-        count += 1
-      end
-    end
-    hash[:count] = count
-  end
-  new_array
-end
